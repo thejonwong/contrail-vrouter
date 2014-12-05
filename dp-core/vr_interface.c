7f206da851dce7fd5ed463cf00abc0e56a4e2e05
Wojciech Zmuda <woz@semihalf.com>
2014-12-05 18:57:17 +0100

    add debugs in vhost_tx()

--- dp-core/vr_interface.c
+++ dp-core/vr_interface.c
@@ -519,6 +519,7 @@ vhost_rx(struct vr_interface *vif, struct vr_packet *pkt,
 static int
 vhost_tx(struct vr_interface *vif, struct vr_packet *pkt)
 {
+    printf("DEBUG: entering vhost_tx();\n");
     int ret;
     struct vr_interface_stats *stats = vif_get_stats(vif, pkt->vp_cpu);
     unsigned short eth_proto, pull_len = 0;
@@ -551,6 +552,7 @@ vhost_tx(struct vr_interface *vif, struct vr_packet *pkt)
                 new_eth = pkt_pull(pkt, pull_len);
                 if (!new_eth) {
                     vr_pfree(pkt, VP_DROP_PULL);
+                    printf("DEBUG: exiting vhost_tx() with return=0\n;");
                     return 0;
                 }
                 memmove(new_eth, eth, (2 * VR_ETHER_ALEN));
@@ -572,6 +574,7 @@ vhost_tx(struct vr_interface *vif, struct vr_packet *pkt)
         stats->vis_oerrors++;
     }
 
+    printf("DEBUG: exiting vhost_tx() with ret=%d\n;", ret);
     return ret;
 }
 
